f8bd3f106f32d20675f4bbc461e2fcd97213e2c5
SegzAyo <jcide22@gmail.com>
2020-10-09 01:20:13 +0000

    user input Work in progress

--- app.py
+++ app.py
@@ -80,7 +80,19 @@ def profile(username):
     #Display current user's username
     username= mongo.db.sellerMDB.find_one(
         {"username" : session["user"]})["username"]
-    return render_template("profile.html", username=username)
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/sign_out")
+def sign_out():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+    
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
